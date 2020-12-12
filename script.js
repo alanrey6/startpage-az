23dfb541ae430f94c2b759ca53fb213ad7a8d3c8
Alan Zavala <alanrey6@protonmail.com>
2020-12-12 15:40:47 -0500

    MAJOR change to code, especially the js and css files to handle the light/dark mode switch

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // get the intitial dark mode state
 let darkMode = localStorage.getItem("darkMode");
 
@@ -5,73 +7,20 @@ let darkMode = localStorage.getItem("darkMode");
 const darkModeToggle = document.getElementById("dark");
 
 const enableDarkMode = () => {
-  // Set background and text colors to dark themes
-  document.documentElement.style.color = "var(--dark-text)";
-  document.documentElement.style.background = "var(--dark-bg-color)";
-  document.getElementsByTagName("nav")[0].style.background =
-    "var(--dark-middle-color)";
-
-  const sectionTag = document.getElementsByTagName("section");
-  for (let i = 0; i < sectionTag.length; i++) {
-    sectionTag[i].style.background = "var(--dark-middle-color)";
-  }
-
-  const linkTag = document.getElementsByTagName("a");
-  for (let i = 0; i < linkTag.length; i++) {
-    linkTag[i].style.color = "var(--dark-text)";
-    linkTag[i].onmouseover = mouseOverDark;
-    linkTag[i].onmouseout = mouseOutDark;
-  }
-
-  document.getElementsByTagName("label")[0].style.color =
-    "var(--primary-color)";
+  document.querySelector("body").classList.remove("theme-light");
+  document.querySelector("body").classList.add("theme-dark");
 
   // Set localstorage to darkMode enabled
   localStorage.setItem("darkMode", "enabled");
 };
 
 const disableDarkMode = () => {
-  // Set background and text colors to light themes
-  document.documentElement.style.color = "var(--light-text)";
-  document.documentElement.style.background = "var(--light-bg-color)";
-  document.getElementsByTagName("nav")[0].style.background =
-    "var(--light-middle-color)";
-
-  const sectionTag = document.getElementsByTagName("section");
-  for (let i = 0; i < sectionTag.length; i++) {
-    sectionTag[i].style.background = "var(--light-middle-color)";
-  }
-
-  const linkTag = document.getElementsByTagName("a");
-  for (let i = 0; i < linkTag.length; i++) {
-    linkTag[i].style.color = "var(--light-text)";
-    linkTag[i].onmouseover = mouseOverLight;
-    linkTag[i].onmouseout = mouseOutLight;
-  }
-
-  document.getElementsByTagName("label")[0].style.color =
-    "var(--secondary-color)";
+  document.querySelector("body").classList.remove("theme-dark");
+  document.querySelector("body").classList.add("theme-light");
 
   // Set localstorage to darkMode disabled
   localStorage.setItem("darkMode", null);
 };
-
-// declare mouse over/our functions
-function mouseOverDark() {
-  this.style.color = "var(--primary-color)";
-}
-
-function mouseOutDark() {
-  this.style.color = "var(--dark-text)";
-}
-
-function mouseOverLight() {
-  this.style.color = "var(--secondary-color)";
-}
-
-function mouseOutLight() {
-  this.style.color = "var(--light-text)";
-}
 
 // checks local storage for status of dark mode
 if (darkMode === "enabled") {
